a5726f84d31343b334e1e7d7c5a4eb82c70b0a7f
brandoncheeys <31523371+brandoncheeys@users.noreply.github.com>
2017-12-14 18:26:46 -0500

    Add files via upload

---
+++ Final Project/finalproject.js
@@ -0,0 +1,45 @@
+ $(document).ready(function(){
+      
+
+
+      $('.carousel').carousel();
+});
+
+
+ $(".thpnbutton").click(function(){
+
+ 	$("p.showtasty").toggle();
+
+	
+
+});
+    
+ $(".ippudobutton").click(function(){
+
+ 	$("p.showippudo").toggle();
+
+	
+
+});
+    $(".CHbutton").click(function(){
+
+ 	$("p.showCH").toggle();
+
+	
+
+});
+    $(".sjfzbutton").click(function(){
+
+ 	$("p.showsjfz").toggle();
+
+	
+
+});
+    $(".friedbutton").click(function(){
+
+ 	$("p.showfried").toggle();
+
+	
+
+});
+    
